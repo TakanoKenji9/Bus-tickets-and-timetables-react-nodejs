da176d39f2bf4d806635159a4dab08f28219494f
TakanoKenji9 <miclej369@gmail.com>
2022-06-01 22:45:43 +0300

    Add navbar using react-bootstrap

---
+++ client/src/components/Header.js
@@ -0,0 +1,32 @@
+import { Navbar, Container, Nav, Link, NavDropdown } from 'react-bootstrap'
+import './Header.css';
+
+function Header() {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand to="/">Даци-Р</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link to="/blog">Новини</Nav.Link>
+                        <Nav.Link to="/">Разписания</Nav.Link>
+                        <NavDropdown title="Услуги" id="collasible-nav-dropdown">
+                            <NavDropdown.Item to="/greece-trip">Екскурзии до Гърция</NavDropdown.Item>
+                            <NavDropdown.Item to="busses-for-rent">Автобуси под наем</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Контакти" id="collasible-nav-dropdown2">
+                            <NavDropdown.Item to="/about">За нас</NavDropdown.Item>
+                            <NavDropdown.Item to="/contacts">Свържете се с нас</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link to="/login">Вход</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header;
