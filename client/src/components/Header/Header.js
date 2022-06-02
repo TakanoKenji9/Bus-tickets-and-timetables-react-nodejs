8710f5873df583f88650adeca8e9f9130a058145
TakanoKenji9 <miclej369@gmail.com>
2022-06-02 15:25:03 +0300

    Set routes into the header menu

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,27 +1,27 @@
-import { Navbar, Container, Nav, Link, NavDropdown } from 'react-bootstrap'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 import './Header.css';
-
+import { NavLink } from "react-router-dom";
 function Header() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
             <Container>
-                <Navbar.Brand to="/">Демо-Д</Navbar.Brand>
+                <NavLink to="/" className="navbar-brand">Демо-Д</NavLink>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link to="/blog">Новини</Nav.Link>
-                        <Nav.Link to="/">Разписания</Nav.Link>
+                        <NavLink className='nav-link' to="/news">Новини</NavLink>
+                        <NavLink className='nav-link' to="/">Разписания</NavLink>
                         <NavDropdown title="Услуги" id="collasible-nav-dropdown">
-                            <NavDropdown.Item to="/greece-trip">Екскурзии до Гърция</NavDropdown.Item>
-                            <NavDropdown.Item to="busses-for-rent">Автобуси под наем</NavDropdown.Item>
+                            <NavLink to="/greece-trip" className="dropdown-item">Екскурзии до Гърция</NavLink>
+                            <NavLink to="/rent" className="dropdown-item">Автобуси под наем</NavLink>
                         </NavDropdown>
                         <NavDropdown title="Контакти" id="collasible-nav-dropdown2">
-                            <NavDropdown.Item to="/about">За нас</NavDropdown.Item>
-                            <NavDropdown.Item to="/contacts">Свържете се с нас</NavDropdown.Item>
+                            <NavLink to="/about" className="dropdown-item">За нас</NavLink>
+                            <NavLink to="/contacts" className="dropdown-item">Свържи се с нас</NavLink>
                         </NavDropdown>
                     </Nav>
                     <Nav>
-                        <Nav.Link to="/login">Вход</Nav.Link>
+                        <NavLink className='nav-link' to="/login">Вход</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
